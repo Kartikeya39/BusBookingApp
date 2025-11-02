e973601ce168fc478ff60b25c996c5593fabd3ae
Kartikeya39 <karthiktelagareddy7@.com>
2025-11-02 23:12:10 +0530

    App Sample View

---
+++ web-app/src/Components/MainBody.jsx
@@ -0,0 +1,66 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+const MainBody = () => {
+  return (
+    <>
+    
+    <header className="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div className="container-fluid">
+          <a className="navbar-brand fw-bold" href="#">
+            BusBookingApp
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+          <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item">
+                <a className="nav-link active" href="#">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Book Tickets
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  About
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </header>
+
+      {/* Main Body */}
+      <main className="container my-5">
+        <div className="text-center">
+          <h1 className="display-5 fw-bold text-primary">Welcome to BusBookingApp</h1>
+          <p className="lead text-muted">
+            Book your bus tickets easily and quickly from anywhere.
+          </p>
+          <button className="btn btn-primary btn-lg mt-3">Get Started</button>
+        </div>
+      </main>
+    </>
+  )
+}
+
+export default MainBody
